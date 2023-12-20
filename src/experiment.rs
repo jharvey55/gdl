23c3a88293a16c54e5771c82e0d4911831b2a258
Atlas1323 <jbcharvey55@gmail.com>
2023-12-20 00:22:49 -0500

    fix: changes random_solution() to directly manipulate members rather than store extra vector

--- src/experiment.rs
+++ src/experiment.rs
@@ -3,14 +3,10 @@ use super::contenders::*;
 // Optimizer functions
 
 pub fn random_generation<V>(pop: &mut Population<V>) {
-    let mut new_pop: Vec<V> = Vec::new();
-
-    for i in 1..pop.pop_size() {
-        new_pop.push(V::random_solution());
+    for mut member in pop.members.iter() {
+        member = V::random_solution();
         pop.increment_eval_count();
     }
-
-    pop.repopulate(new_pop);
 }
 
 
